24f823bba8519cbb8cabee46a4a9a4ab8f547bfb
Stephen Prosser <stephenaprosser@gmail.com>
2014-06-23 15:02:50 -0500

    vectors ftw

--- multi_if.clj
+++ multi_if.clj
@@ -1,5 +1,8 @@
-(defmacro multi [test & {:keys [then else]}]
-  (let [x test]
-  `(if ~x
-    (prn ~then)(prn ~else)
-)))
+(defmacro multi-if [test & forms]
+  (let [forms (vec (partition-by #{:else :then} forms))
+        [then else] (if (= :then (first (get forms 0)))
+                      [(get forms 1) (get forms 3)]
+                      [(get forms 3) (get forms 1)])]
+    `(if ~test
+       (do ~@then)
+       (do ~@else))))
